4192b15fae8fbada852868bcc3358823029b0f4b
Vasanth <vasanth@xcaliber.health>
2025-03-06 16:38:39 +0530

    wrap agent in api

--- xc-demo/main.py
+++ xc-demo/main.py
@@ -1,28 +1,29 @@
+import uvicorn
 from langchain_openai import ChatOpenAI
 from browser_use import Agent
 from dotenv import load_dotenv
+import asyncio
+from browser_use import BrowserConfig, Browser
+from fastapi import FastAPI
+
 load_dotenv()
 
-import asyncio
-from browser_use import BrowserConfig,Browser
+app = FastAPI()
 
 # Basic configuration
 config = BrowserConfig(
     headless=False,
-    disable_security=True,
-    highlight_elements=False,
-    wait_for_network_idle_page_load_time=3.0,
-    maximum_wait_page_load_time=5.0
+    disable_security=True
 )
 
 browser = Browser(config=config)
 
-llm =  model=ChatOpenAI(
-            model= "gpt-4o",
-            temperature=0.0,
-            base_url="https://xcaliber-litellm.xcaliberhealth.io/",
-            api_key="sk-DTEDkyDf_sUw14xyRrhuGw",
-        )
+llm = ChatOpenAI(
+    model="gpt-4o",
+    temperature=0.0,
+    base_url="https://xcaliber-litellm.xcaliberhealth.io/",
+    api_key="sk-DTEDkyDf_sUw14xyRrhuGw",
+)
 
 async def main():
     agent = Agent(
@@ -33,4 +34,19 @@ async def main():
     result = await agent.run()
     print(result)
 
-asyncio.run(main())
+@app.post("/start_vnc")
+async def start_vnc():
+    agent = Agent(
+        task="Compare the price of gpt-4o and DeepSeek-V3",
+        llm=llm,
+        browser=browser,
+    )
+    result = await agent.run()
+    return {"result": result}
+
+@app.get("/hello")
+async def hello():
+    return {"Hello": "World"}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
